1549f10cbbb1456e33b4edd33c5c065f5ee1b045
vboxuser <vboxuser@Inception.42.fr>
2025-09-19 15:24:36 +0100

    From VM.

--- srcs/requirements/bonus/ftp/tools/start_ftp.sh
+++ srcs/requirements/bonus/ftp/tools/start_ftp.sh
@@ -6,7 +6,7 @@ echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
 
 # Set permissions
 chown -R $FTP_USER:$FTP_USER /var/www/wordpress
-chmod -R 755 /var/www/wordpress
+chmod -R 777 /var/www/wordpress/
 
 # Create vsftpd user list
 echo $FTP_USER > /etc/vsftpd.userlist
