a02f287a829034c26fdfa4067fbac8868c1e0126
lassachraf <lassachraf1@gmail.com>
2025-09-21 18:36:39 +0100

    Commiting the added bonus services, and modifying others.

--- srcs/requirements/bonus/ftp/tools/start_ftp.sh
+++ srcs/requirements/bonus/ftp/tools/start_ftp.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
 # Create FTP user
-useradd -m -d /var/www/wordpress -s /bin/bash $FTP_USER
+useradd -d /var/www/wordpress -s /bin/bash $FTP_USER
 echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
-
-# Set permissions
-chown -R $FTP_USER:$FTP_USER /var/www/wordpress
-chmod -R 777 /var/www/wordpress/
-
-# Create vsftpd user list
-echo $FTP_USER > /etc/vsftpd.userlist
 
 # Start vsftpd
 exec vsftpd /etc/vsftpd.conf
---
+++ srcs/requirements/bonus/website/tools/script.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#3B82F6',
+                secondary: '#1E40AF',
+                accent: '#10B981',
+                dark: '#1F2937',
+                light: '#F9FAFB'
+            }
+        }
+    }
+}
+
+
